99193762b10559c21d582e348db98be8c012a57e
Marceillo De Wet <marceillo@hotmail.com>
2024-10-07 10:02:09 +0000

    Changed the debug to False

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = False
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'boutiqueadov2-da8587bc9277.herokuapp.com',
